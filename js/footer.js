51a1ab262e740af42c6c76662df5b194394d3fe1
faridaalin <56302698+faridaalin@users.noreply.github.com>
2020-03-23 10:19:43 +0100

    tidy up

--- js/footer.js
+++
@@ -1,5 +0,0 @@
-let year = new Date().getFullYear();
-let date = `${year} &copy; Shadowball. All rights reserved.`
-
-const footer = document.querySelector('#copywrightYear').innerHTML = date;
-
--- js/menu.js
+++
@@ -1,8 +0,0 @@
-const navBar = document.querySelector('.nav-bar');
-console.dir(navBar)
-const navbarToggle = document.querySelector('.navbar-toggle');
-console.dir(navbarToggle)
-
-navbarToggle.addEventListener('click', () => {
-    navBar.classList.toggle('nav-active');
-})
